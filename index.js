7a4f95dce5f860de0ab1fd6de010d0b0fd26539a
gourav-codes99 <grcodes99@gmail.com>
2024-02-13 19:29:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var Student = [3,9,18,3,28,6,17,9,3]
+
+console.log(Student.sort())
+
+// let i = 0;
+
+// for (i<Student.length-1 ; i++){
+
+//     for (let j = i+1 ; j<Student.length;j++){
+
+//     }
+// }
---
+++ pyramid.js
@@ -0,0 +1,12 @@
+int n =5 ;
+ int st = 1; 
+
+for (let i = 0; i <n ; i++){
+    for (j = 0; j = st ; j++){
+        console.log("\t");
+    }
+
+    for (let j = 0; j=st ; j++){
+        console.log("\t")
+    }
+}
